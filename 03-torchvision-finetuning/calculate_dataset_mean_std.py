c24b079e2516694845b77275c6e2c3e5b4abd960
curt <curt.k@kakaomobility.com>
2021-09-17 19:13:38 +0900

    calculate_dataset_mean_std추가

--- 03-torchvision-finetuning/calculate_dataset_mean_std.py
+++ 03-torchvision-finetuning/calculate_dataset_mean_std.py
@@ -15,17 +15,25 @@ def main():
         collate_fn=utils.collate_fn     # image들의 size가 다른 경우에 필요하다.
     )
 
+    pixel_count = 0
     psum = torch.tensor([0.0, 0.0, 0.0])
     psum_sq = torch.tensor([0.0, 0.0, 0.0])
 
     for images, targets in data_loader:
-        #print(images)
-        image = images[0]
-        print(image.shape)
-        print(image.sum(axis=[1, 2]))
+        for image in images:
+            h, w = image.shape[1], image.shape[2]
+            pixel_count += h*w
+            psum += image.sum(axis=[1, 2])
+            psum_sq += (image ** 2).sum(axis=[1, 2])
 
-        #target = targets[0]
-        #print(image.dtype, image.shape, target['boxes'].shape)
+    # mean and std
+    total_mean = psum / pixel_count
+    total_var = (psum_sq / pixel_count) - (total_mean ** 2)
+    total_std = torch.sqrt(total_var)
+
+    # output
+    print('mean: ' + str(total_mean))
+    print('std:  ' + str(total_std))
 
 
 main()
