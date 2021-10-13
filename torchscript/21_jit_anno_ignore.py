f92346a569de764b782c60e70a2ba9c584abb9b7
curt.k <curt.k@kakaocorp.com>
2021-10-13 19:25:01 +0900

    minor

--- torchscript/21_jit_anno_ignore.py
+++ torchscript/21_jit_anno_ignore.py
@@ -14,8 +14,31 @@ class MyModule(nn.Module):
         return x
 
 
-m = torch.jit.script(MyModule())
+def test_scripted_module():
+    m = torch.jit.script(MyModule())
 
-# This is OK since `training_method` is not saved, the call is replaced
-# with a `raise`.
-m.save("m.pt")
+    # This is OK since `training_method` is not saved, the call is replaced
+    # with a `raise`.
+    m.save("m.pt")
+
+    sample = torch.arange(5)
+    try:
+        print(m(sample))
+    except torch.jit.Error as err:
+        print('catched')
+
+
+def test_raw_module():
+    sample = torch.arange(5)
+
+    mym = MyModule()
+    assert mym.training
+    mym.eval()
+    assert not mym.training
+    print(mym(sample))
+    mym.train()
+    assert mym.training
+
+
+test_raw_module()
+test_scripted_module()
