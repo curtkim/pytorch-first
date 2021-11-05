9bbee34e3b815b4cf438176dcbf4bc8040b5997b
curt <curt.k@kakaomobility.com>
2021-11-05 11:05:53 +0900

    jupyter ascending 테스트

---
+++ example.sync.py
@@ -0,0 +1,24 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.3.4
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %%
+print("Hello World!")
+
+# %%
+import numpy as np
+np.arange(1, 8)
+
+# %%
+np.arange(1,2)
+
