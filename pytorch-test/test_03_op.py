8e0914d37a27fbd0b70f67192c32b18dd420f88a
curt.k <curt.k@kakaocorp.com>
2021-12-27 14:53:26 +0900

    object detection metric 수정

--- pytorch-test/test_03_op.py
+++ pytorch-test/test_03_op.py
@@ -6,3 +6,7 @@ def test_squeeze():
     a = torch.ones((1, 6, 4, 1))
     assert torch.Size([6, 4]) == a.squeeze().shape
 
+
+def test_trapz():
+    Y = torch.tensor([1, 4, 9])
+    assert (1 + 3 / 2) + (4 + 5 / 2) == torch.trapz(Y)
