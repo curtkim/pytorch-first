1f6d42e95c847b1a49654e5390fffa43c7dcbd9d
curt <curt.k@kakaomobility.com>
2022-01-17 13:04:22 +0900

    catalyst additive metry수정

--- catalyst-first/12_additive_metric.py
+++ catalyst-first/12_additive_metric.py
@@ -1,9 +1,11 @@
+from pytest import approx
 import torch
 from catalyst import metrics
+import statistics
 
 
 def test_additive_metric_update():
-    metric = metrics.AdditiveMetric(True)
+    metric = metrics.AdditiveMetric()
     metric.update(1, 1)
     metric.update(2, 1)
     metric.update(3, 1)
@@ -11,9 +13,10 @@ def test_additive_metric_update():
 
 
 def test_additive_metric_update_batch():
-    metric = metrics.AdditiveMetric(True)
+    metric = metrics.AdditiveMetric()
     metric.update(1, 2)
     metric.update(2, 2)
     metric.update(3, 2)
     mean, std = metric.compute()
     assert 2.0 == mean
+    assert std == statistics.stdev([1, 1, 2, 2, 3, 3])
