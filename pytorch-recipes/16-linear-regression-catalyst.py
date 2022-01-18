8a62e8e0f1167dfb12b4d7ea7ea13ccf0fd8caaf
curt <curt.k@kakaomobility.com>
2022-01-18 12:46:13 +0900

    catalyst에서 multi cpu logging성공

--- pytorch-recipes/16-linear-regression-catalyst.py
+++ pytorch-recipes/16-linear-regression-catalyst.py
@@ -4,14 +4,12 @@ import os
 
 import numpy as np
 import torch
+from catalyst.engines import DistributedDataParallelEngine
 from catalyst.core import Callback, CallbackOrder, IRunner
 from torch import optim
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from torch.utils.data import Dataset
-
 from catalyst import dl
-
-print('os.getpid()', os.getpid())
 
 
 class MyDataset(Dataset):
@@ -43,24 +41,6 @@ class linearRegression(torch.nn.Module):
         out = self.linear(x)
         return out
 
-
-dataset = MyDataset(10)
-loaders = {
-    "train": torch.utils.data.DataLoader(dataset, batch_size=2)
-}
-
-inputDim = 1        # takes variable 'x'
-outputDim = 1       # takes variable 'y'
-learningRate = 0.01
-num_epochs = 1
-
-model = linearRegression(inputDim, outputDim)
-
-criterion = torch.nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=learningRate)
-scheduler = optim.lr_scheduler.MultiStepLR(optimizer, [5, 8], gamma=0.3)
-
-
 class DebugCallback(Callback):
     def __init__(self):
         super().__init__(order=CallbackOrder.external)
@@ -91,29 +71,52 @@ class DebugCallback(Callback):
         assert 'loss' in runner.batch_metrics
 
 
-runner = dl.SupervisedRunner(input_key="inputs", target_key="targets", output_key="outputs")
-runner.train(
-    model=model,
-    criterion=criterion,
-    optimizer=optimizer,
-    scheduler=scheduler,
-    loaders=loaders,
-    logdir="logs",
-    num_epochs=num_epochs,
-    verbose=True,
-    callbacks=[
-        DebugCallback()
-    ]
-)
+def main():
+    print('os.getpid()', os.getpid())
 
-model_cpu = model.cpu()
-with torch.no_grad():   # we don't need gradients in the testing phase
-    predicted = model_cpu(torch.from_numpy(dataset.x_train)).data.numpy()
-    print(predicted)
+    dataset = MyDataset(10)
+    loaders = {
+        "train": torch.utils.data.DataLoader(dataset, batch_size=2)
+    }
+
+    inputDim = 1        # takes variable 'x'
+    outputDim = 1       # takes variable 'y'
+    learningRate = 0.01
+    num_epochs = 1
+
+    model = linearRegression(inputDim, outputDim)
+
+    criterion = torch.nn.MSELoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=learningRate)
+    scheduler = optim.lr_scheduler.MultiStepLR(optimizer, [5, 8], gamma=0.3)
+
+    runner = dl.SupervisedRunner(input_key="inputs", target_key="targets", output_key="outputs")
+    runner.train(
+        engine=DistributedDataParallelEngine(),
+        model=model,
+        criterion=criterion,
+        optimizer=optimizer,
+        scheduler=scheduler,
+        loaders=loaders,
+        logdir="logs",
+        num_epochs=num_epochs,
+        verbose=True,
+        callbacks=[
+            DebugCallback()
+        ]
+    )
+
+    model_cpu = model.cpu()
+    with torch.no_grad():   # we don't need gradients in the testing phase
+        predicted = model_cpu(torch.from_numpy(dataset.x_train)).data.numpy()
+        print(predicted)
+
+    # plt.clf()
+    # plt.plot(dataset.x_train, dataset.y_train, 'go', label='True data', alpha=0.5)
+    # plt.plot(dataset.x_train, predicted, '--', label='Predictions', alpha=0.5)
+    # plt.legend(loc='best')
+    # plt.show()
 
 
-plt.clf()
-plt.plot(dataset.x_train, dataset.y_train, 'go', label='True data', alpha=0.5)
-plt.plot(dataset.x_train, predicted, '--', label='Predictions', alpha=0.5)
-plt.legend(loc='best')
-plt.show()
+if __name__ == '__main__':
+    main()
