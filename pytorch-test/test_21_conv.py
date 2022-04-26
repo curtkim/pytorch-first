313abb057b73c724d1054ae20c94b0b2823f4a7c
curt <curt.k@kakaomobility.com>
2022-04-26 12:59:12 +0900

    pytorch-test test_conv2d_functional추가

--- pytorch-test/test_21_conv.py
+++ pytorch-test/test_21_conv.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.autograd import Variable
 
 
@@ -30,3 +31,21 @@ def test_conv2d_non_square_kernel():
     input = torch.randn(20, 16, 50, 100)
     output = m2(input)
     assert torch.Size([20, 33, int((50 - 3 + 8)/2)+1, int((100 - 5 + 2*2))+1]) == output.shape
+
+
+def test_conv2d_functional():
+    # input: (batch_size, in_channels, height, width)
+    # weight: (out_channels, in_channels, kernel_height, kernel_width)
+
+    input = torch.reshape(torch.arange(0, 25, dtype=torch.float32), (1, 1, 5, 5))
+    filter = torch.ones(1, 1, 3, 3, dtype=torch.float32) / 9
+    # print(input)
+    # print(F.conv2d(input, filter, padding=1))
+    torch.testing.assert_allclose(
+        F.conv2d(input, filter, padding=0),
+        torch.tensor([[[
+            [6, 7, 8],
+            [11, 12, 13],
+            [16, 17, 18],
+        ]]], dtype=torch.float32)
+    )
